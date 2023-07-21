a8986d38ed26cfa186a50e06fa4083666e79b241
chand1997 <chanduchallahalli@gmail.com>
2023-07-21 14:18:06 +0530

    changes2

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -13,6 +13,22 @@ public class BankAccount {
 
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
     public double getBalance() {
         return balance;
     }
